638a2251e20a318c65e5fe37eeda9e307a1b1383
zeniquelgianniivan <zeniquelgianniivan@chaco.gov.ar>
2023-08-28 23:12:31 -0300

    cambios en urls vich_uploader y pagoController, comentamos register

--- src/Controller/Admin/PagoCrudController.php
+++ src/Controller/Admin/PagoCrudController.php
@@ -114,7 +114,7 @@ class PagoCrudController extends AbstractCrudController
             ->addHtmlContentToHead('<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
                                     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
                                     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>')
-            ->addJsFile('/public/front/js/pago.js')
+            ->addJsFile('/gestionComprobantesHum/public/front/js/pago.js')
 
         ;
     }
