e95513b1ef68e858661a8e3a1e0ece42e3865e2a
zeniquelgianniivan <zeniquelgianniivan@chaco.gov.ar>
2023-09-01 13:35:09 -0300

    redireccion custom mediante subcriber pago luego de un new/edit pago hacia template mispagos

--- src/EventSubscriber/PagoSubscriber.php
+++ src/EventSubscriber/PagoSubscriber.php
@@ -3,8 +3,14 @@
 namespace App\EventSubscriber;
 
 use App\Entity\Pago;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityUpdatedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Security;
 
 class PagoSubscriber implements EventSubscriberInterface
@@ -13,10 +19,24 @@ class PagoSubscriber implements EventSubscriberInterface
      * @var Security
      */
     private $security;
+    /**
+     * @var AdminUrlGenerator
+     */
+    private $adminUrlGenerator;
     
-    public function __construct(Security $security)
+    public function __construct(Security $security, AdminUrlGenerator $adminUrlGenerator) //
     {
         $this->security = $security;
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BeforeEntityPersistedEvent::class => 'onBeforeEntityPersistedEvent',
+            AfterEntityPersistedEvent::class => 'onAfterEntityPersistedEvent',
+            AfterEntityUpdatedEvent::class => 'onAfterEntityUpdatedEvent',
+        ];
     }
 
     public function onBeforeEntityPersistedEvent(BeforeEntityPersistedEvent $event): void
@@ -28,10 +48,16 @@ class PagoSubscriber implements EventSubscriberInterface
         } 
     }
 
-    public static function getSubscribedEvents(): array
+    public function onAfterEntityPersistedEvent(): Response
     {
-        return [
-            BeforeEntityPersistedEvent::class => 'onBeforeEntityPersistedEvent',
-        ];
+        $url =  $this->adminUrlGenerator->setController('App\\Controller\\Admin\\UserCrudController')->setAction('obtenerPagosUsuario');
+        return (new RedirectResponse($url))->send(); 
     }
+
+    public function onAfterEntityUpdatedEvent(): Response
+    {
+        $url =  $this->adminUrlGenerator->setController('App\\Controller\\Admin\\UserCrudController')->setAction('obtenerPagosUsuario');
+        return (new RedirectResponse($url))->send();  
+    }
+
 }
