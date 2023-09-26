ff1420766b5be06cc5c556d3ed2ccd1790858fd2
Gianni Ivan Zeniquel <zeniquelgianniivan@chaco.gob.ar>
2023-09-26 11:46:34 -0300

    fix misPagos luego de generar un pago, y cuando accedo desde misCursos

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -89,7 +89,11 @@ class UserCrudController extends AbstractCrudController
     public function configureAssets(Assets $assets): Assets
     {
         return $assets
-            ->addHtmlContentToHead('<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>')
+            ->addHtmlContentToHead('<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+                                    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+                                    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+                                    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css"/>
+                                    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>')
             ->addJsFile('/gestionComprobantesHum/public/front/js/user.js')
             ->addJsFile('/gestionComprobantesHum/public/front/js/base.js');
     }
